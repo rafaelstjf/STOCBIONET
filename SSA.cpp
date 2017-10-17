baa18be10f36c6c43b40ce67048e38fd06955cea
rafaelstjf <rafaelstjf@hotmail.com>
2017-10-17 15:45:56 -0200

    SSA

---
+++ SSA.cpp
@@ -0,0 +1,14 @@
+#include "SSA.h"
+#include <iostream>
+#include <stdlib.h>
+#include "DependencyGraph.h"
+
+SSA::SSA()
+{
+    //ctor
+}
+
+SSA::~SSA()
+{
+    //dtor
+}
---
+++ SSA.h
@@ -0,0 +1,16 @@
+#ifndef SSA_H
+#define SSA_H
+
+
+class SSA
+{
+    public:
+        SSA();
+        virtual ~SSA();
+
+    protected:
+
+    private:
+};
+
+#endif // SSA_H
