218081ec3c71293da0984d685ea3f36d35103bf1
rafaelstjf <rafaelstjf@hotmail.com>
2018-02-26 15:19:00 -0300

    inserção do diretório

---
+++ GerarGRN/SpecieQuantity.cpp
@@ -0,0 +1,26 @@
+#include "SpecieQuantity.h"
+
+SpecieQuantity::SpecieQuantity(){
+quantity = 0;
+
+}
+SpecieQuantity::~SpecieQuantity(){
+}
+int SpecieQuantity::getQuantity(){
+    return quantity;
+}
+void SpecieQuantity::setQuantity(int quant){
+    quantity = quant;
+}
+Delay* SpecieQuantity::getDelay(){
+    return delay;
+}
+Specie* SpecieQuantity::getSpecie(){
+    return specie;
+}
+void SpecieQuantity::setSpecie(Specie* sp){
+   specie = sp;
+}
+void SpecieQuantity::setDelay(Delay* de){
+    delay = de;
+}
